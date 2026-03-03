1f3e7c365febfaa91e711df2abea88136e524efb
YK <yk48631@ly.com>
2026-03-03 11:19:23 +0800

    Clear all files from main-1 branch

--- renderapi/app/api/router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.routes import markitdown
-
-api_router = APIRouter()
-api_router.include_router(markitdown.router, prefix="/tools/markitdown", tags=["markitdown"])
--- services/markitdown/src/markitdown_tool/__init__.py
+++
@@ -1,3 +0,0 @@
-from markitdown_tool.core import execute_markitdown
-
-__all__ = ["execute_markitdown"]
